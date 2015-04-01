5b33884b006bf01275575d2f85938f56cad2548b
Paul Burgwardt <Paul@PaulBurgwardt.me>
2015-03-31 22:54:55 -0400

    Converted LOGIN to use PDO.

--- Classes/COMM444/Projects/Event Calendar/login.php
+++ Classes/COMM444/Projects/Event Calendar/login.php
@@ -1,41 +1,36 @@
 <?php
 	include ('globals.php');
-	$globalGeneralErr="";
+	$error="";
 	if(isset($_POST['login']))
 	{
 		if($_POST['accname']!=NULL)
 		{
 			if($_POST['accpass']!=NULL)
 			{
-				$link = mysql_connect($DB_HOST,$DB_USER,$DB_PASS);
-				if(!$link){
-					$globalGeneralErr = "Unable to connect at this time.\n";
+				if(!$conn){
+					$error = "Unable to connect at this time.\n";
 				}
 				else {
-					mysql_select_db($DB_DB) or die(mysql_error()); 
 					$account = $_POST['accname'];
 					$password = $_POST['accpass'];			
-					$query_check_auth = "select * from User where user_name='$account' and password='$password';";
-					$result_auth = mysql_query($query_check_auth,$link);
-					if($result_auth!=NULL){
-						$row = mysql_fetch_array($result_auth); 
-						$num_results = mysql_num_rows($result_auth); 
-						if($num_results>0) { 
-							session_start();
-							// GET THE USER_ID
-							$_SESSION['uid']=$row['user_ID'];
-							$_SESSION['user']=$account;
-							$_SESSION['month']=date('n');
-							$_SESSION['year']=date('Y');
-							header("Location: /home.php");
-						} // closing valid credentials
-						else{
-							$globalGeneralErr = "Invalid credentials<br>";
-						} // closing invalid credentials
+					// Prepare Query
+					$result = $conn->prepare("SELECT * FROM User WHERE user_name=:Us3Rn4M3 AND password=:Pa55W0rD");
+					$result->bindParam(':Us3Rn4M3', $account);
+					$result->bindParam(':Pa55W0rD', $password);
+					// Execute Query
+					$result->execute();
+					$rows = $result->fetch(PDO::FETCH_NUM);
+					if($rows > 0){
+						// GET THE USER_ID
+						//$_SESSION['uid']=$row['user_ID'];
+						$_SESSION['user']=$account;
+						$_SESSION['month']=date('n');
+						$_SESSION['year']=date('Y');
+						header("Location: home.php");
 					} // closing result query for authentication 
 					else
 					{
-						$globalGeneralErr = "Check for authentication was not successful<br>";
+						$error = "Username or Password were not found in the system.<br>";
 					} // closing result for query authentication invalid check
 				} // closing successful connection to server
 			} // closing accpass post
@@ -45,23 +40,10 @@
 <html>
 	<head>
 		<title>Login</title>
-			<style>
-				a,a:visited{
-					color: #80D9FF;
-					text-decoration: none;
-				}
-				a:hover{
-					color: #00B2FF;
-					text-decoration: none;
-				}
-				a:active{
-					color: #00B2FF;
-					text-decoration: none;
-				}
-			</style>
+		<link rel="stylesheet" type="text/css" href="style.css">
 	</head>
 	<body>
-		<div style="width: 500px; height: auto; border: 1px solid #D3D3D3; margin: auto; padding: 10px;">
+		<div id="container">
 		<b><h3><?php echo $title; ?> | Login</h3></b>
 		<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
 			<!-- TABLE LOGIN -->
@@ -74,7 +56,7 @@
 				</tr>
 				<tr><td><input type="submit" name="login" value="Login"></td></tr>
 			</table>
-			<font color="red"><?php echo $globalGeneralErr; ?></font>
+			<font color="red"><?php echo $error; ?></font>
 			<p>Not registered? <a href="register.php">Click here to sign up!</a></p>
 		</form>
 		</div>
